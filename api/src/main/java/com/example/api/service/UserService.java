ca853f3f9896627b3ec02530c13e4928f08e3b71
ziyuan <1115620874@qq.com>
2021-10-23 17:05:29 +0800

    some profiles

---
+++ api/src/main/java/com/example/api/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.api.service;
+
+/**
+ * @author ziyuan
+ * @Description
+ * @createTime 2021年10月22日 20:56:00
+ */
+public interface UserService {
+    public String login(String username);
+}
